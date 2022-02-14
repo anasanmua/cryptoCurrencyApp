575f97160afc062aed156675cb558042dc264315
VmMad <88061365+VmMad@users.noreply.github.com>
2022-02-14 18:40:11 +0100

    fixed layout rendering

--- middleware/user-guard.js
+++ middleware/user-guard.js
@@ -5,12 +5,21 @@ const setLoggedUser = (req, res, next) => {
     if (req.session) {
         if (req.session.currentUser) {
             req.app.locals.loggedUser = req.session.currentUser
+            req.app.locals.roleBEGINNER = req.session.currentUser.role === "BEGINNER"
+            req.app.locals.roleADVANCED = req.session.currentUser.role === "ADVANCED"
+            req.app.locals.roleINTERMEDIATE = req.session.currentUser.role === "INTERMEDIATE"
             next()
         } else {
             req.app.locals.loggedUser = undefined
+            req.app.locals.roleBEGINNER = false
+            req.app.locals.roleADVACED = false
+            req.app.locals.roleINTERMEDIATE = false
             next()
         }
     } else {
+        req.app.locals.roleBEGINNER = false
+        req.app.locals.roleADVACED = false
+        req.app.locals.roleINTERMEDIATE = false
         req.app.locals.loggedUser = undefined
         next()
     }
