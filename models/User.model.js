e493039335fda81034f75c4c96f93ab8fd2daaaf
ana.sanmua@gmail.com <86674956+anasanmua@users.noreply.github.com>
2022-02-14 11:14:00 +0100

    model

---
+++ models/User.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+
+    name: String,
+    description: String,
+    image: String,
+    password: String,
+
+    role: {
+      type: String,
+      enum: ['BEGGINER', 'INTERMEDIATE', 'ADVANCED'],
+      default: 'BEGGINER',
+
+    },
+
+    favCryptoCurrency: [String],
+  },
+  {
+
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("cryptoapp JS imported successfully!");
+  },
+  false
+);
